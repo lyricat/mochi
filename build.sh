0b9aef1d10e7cc0938cd12303c03842f99fd839e
Shellex Wai <5h3ll3x@gmail.com>
2012-06-20 20:28:07 +0800

    dark theme

--- build.sh
+++ build.sh
@@ -5,15 +5,18 @@ then
 	mkdir "$DIST"
 	mkdir "$DIST/css"
 	mkdir "$DIST/js"
+	mkdir "$DIST/image"
 else
 	if [ ! -e "$DIST/css" ]; then mkdir "$DIST/css"; fi
 	if [ ! -e "$DIST/js" ]; then mkdir "$DIST/js"; fi
+	if [ ! -e "$DIST/image" ]; then mkdir "$DIST/image"; fi
 fi
 
 echo "Generate..."
 lessc css/mochi.less > dist/css/mochi.css
 coffee -c -o dist/js/ js/mochi.coffee 
 cp mochi.html dist/index.html
+cp -a -r image dist/
 
 echo "Compress..."
 which yui-compressor && find "$DIST" -name '*.css' -exec yui-compressor -o "{}" "{}" \;
